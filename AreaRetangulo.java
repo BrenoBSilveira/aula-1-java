71baae362b28012e904bd5b21429c6200b219f73
Breno Silveira <breno.silveira@gamasti.com.br>
2022-02-10 13:22:29 -0300

    primeiro commit

---
+++ AreaRetangulo.java
@@ -0,0 +1,19 @@
+
+public class AreaRetangulo
+{
+    /* 
+     Area Retangulo = base x altura
+    */
+    
+    public static void main(String[] args){
+        float base = 10.23f;
+        float altura = 54.68f;
+        float area = 0;
+        
+        area = base * altura;
+        System.out.println(area);
+        System.out.println("Garcia the John");
+        System.out.println("Sifinis the Bre");
+        System.out.println("Gigis the Xanxis");
+    }
+}
---
+++ CotacaoMoeda.java
@@ -0,0 +1,10 @@
+public class CotacaoMoeda
+{
+    public static void main(String[] args){
+        float kwanza = 10f;
+        float dolar = 5.24f;
+        
+        float mangos = kwanza * dolar;
+        System.out.println("Total de mangos: "+ mangos);
+    }
+}
